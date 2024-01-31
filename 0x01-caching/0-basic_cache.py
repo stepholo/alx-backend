96c9ae4b282922fc0de6cff0e33541739fe46c8b
stepholo <oloobrian89@gmail.com>
2024-01-31 19:54:16 +0000

    first cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Defines a basic cache without a limit
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
